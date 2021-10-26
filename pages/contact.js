31773c4dcba7bd1fd33ceccef1dacd3b536e28cd
Timur <megapers@gmail.com>
2021-10-25 23:02:29 -0700

    Added API routes and Mongo connection

--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,16 @@
+import ContactForm from "../components/contact/contact-form";
+import Head from "next/head";
+import { Fragment } from "react";
 function ContactPage() {
-  return "";
+  return (
+    <Fragment>
+      <Head>
+        <title>Contact Me</title>
+        <meta name="description" content="Send me your messages"/>
+      </Head>
+      <ContactForm />
+    </Fragment>
+  );
 }
 
 export default ContactPage;
